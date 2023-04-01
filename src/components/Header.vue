502c81b8082d56f3ed03046176544b674f80ad50
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-04-01 12:10:54 -0300

    src

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,13 @@
 <template>
   <header class="bg-dark-purple w-full flex items-center justify-center">
-    <div class="container px-5 pt-14 pb-14 flex items-center justify-between">
+    <nav class="container px-5 pt-14 pb-14 flex items-center justify-between">
       <img class="logo" src="../assets/images/logo-light.svg" alt="" width="99" height="24" />
-      <a href="/" class="text-white border-b-4 border-eucaplyptus text-lg hover:text-eucaplyptus transition-colors duration-500">Apply for access</a>
-    </div>
+      <a href="/" class="text-base/8 text-white border-b-4 border-eucaplyptus hover:text-eucaplyptus transition-colors duration-500">Apply for access</a>
+    </nav>
   </header>
 </template>
 
 <style scoped>
-a {
-  line-height: 32px;
-}
 @media (min-width: 64.25rem) {
   .container {
     padding: 3.5rem 10.3125rem !important;
